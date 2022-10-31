2bd03b3119edf5d82662fd80a8b8d15e59be6397
julianagomez21 <jgomezgal25@gmail.com>
2022-10-31 17:42:11 -0500

    Se agregan las carpetas para la carga de archivos de zonas, parques y puestos de comidas

--- src/config/general-config.ts
+++ src/config/general-config.ts
@@ -3,7 +3,10 @@ export namespace GeneralConfig {
   export const metodo_validar_jwt = 'validate-token';
   export const adminRolId = '633aedcc38f68246c4b8bcb7';
   export const carpetaArchivosAtracciones = '../../archivos/atracciones';
-
-  export const campoDeAtraccion = 'file';
+  export const carpetaArchivosZonas = '../../archivos/zonas';
+  export const carpetaArchivosParquesLogos = '../../archivos/parquesLogos';
+  export const carpetaArchivosParquesMapas = '../../archivos/parquesMapas';
+  export const carpetaArchivosPuestos = '../../archivos/puestosComidas';
+  export const campoDeArchivo = 'file';
   export const extensionesImagenes = ['.SVG', '.PNG', '.JPG', '.JPEG'];
 }
